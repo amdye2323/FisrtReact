c1c24b444ff63a370c54ab76578341c7f2cd3dad
KimTaeyoug <amdye2323@naver.com>
2020-05-10 22:29:58 +0900

    youtube

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React ,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyBF9g_yjIFG4pcFFdnBZZybwPLSuKf0KXE';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import _ from 'lodash';
+
+class App extends Component
+{
+    constructor(props) 
+    {
+            super(props);
+
+            this.state = 
+            { 
+                videos : [],
+                selectedVideo : null
+            };
+          
+           this.videoSearch('surf');
+    }
+
+    videoSearch(term) 
+    {
+        YTSearch({key : API_KEY,term : term},(videos)  =>
+        {
+            this.setState({
+                 videos : videos,
+                 selectedVideo : videos[0]
+                });
+            //this.setState({videos : videos});
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce( (term) => {this.videoSearch(term) }, 300);
+        return(
+    <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+        onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} 
+        />
+        
+    </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
